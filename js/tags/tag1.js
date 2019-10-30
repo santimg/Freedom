c6eee6ce06e02f865300f2f7356b5c97fbb940f7
smg <santiago.martinezgarcia@telefonica.com>
2019-10-30 14:25:48 +0100

    content tagging update

--- js/tags/tag1.js
+++ js/tags/tag1.js
@@ -1,2 +1,2 @@
-document.write('<iframe scrolling="no" frameborder="0" style="width:300px;height:250px" src="https://adserver.freedom.tikiphee.com/ad-iframe.html"></iframe>');
+document.write('<iframe scrolling="no" frameborder="0" style="width:350px;height:250px" src="https://adserver.freedom.tikiphee.com/ad-iframe.html"></iframe>');
 
